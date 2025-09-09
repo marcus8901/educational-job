8c0ee0c90a4090e242017945a1b3e0a1044fe866
nenofx8901 <moneywithnenofx@gmail.com>
2025-09-09 19:26:32 +0100

    first commit

--- northfieldbank/settings.py
+++ northfieldbank/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")  # use env in Render
 DEBUG = False  # Set to False for production deployment
-ALLOWED_HOSTS = ['login.barclaysbank.myaccountcenter.online', 'www.login.barclaysbank.myaccountcenter.online', 'updated-bank-education.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
